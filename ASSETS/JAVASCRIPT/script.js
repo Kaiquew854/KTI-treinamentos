6310cb4b21e61a87e15ffa394dbb1f2d99817e0d
Kaiquew854 <80990220+Kaiquew854@users.noreply.github.com>
2022-11-03 16:12:22 -0300

    ADIÇÃO DO SLIDESHOW E MELHORIAS NO CSS

---
+++ ASSETS/JAVASCRIPT/script.js
@@ -0,0 +1,38 @@
+let qtdSld;
+let curSld=0;
+qtdSld=document.querySelectorAll('.slider--item').length;
+document.querySelector('.main--slider').style.width=`calc(100vw * ${qtdSld})`;
+
+//voltar
+document.querySelector('.btn1').addEventListener('click', ()=>{
+    curSld--;
+    if( curSld < 0){
+        curSld = qtdSld - 2;
+    }
+    updateMagin();
+})
+//avancar
+document.querySelector('.btn2').addEventListener('click', ()=>{
+    curSld++;
+    if (curSld > qtdSld-1){
+        curSld = 0;
+    }
+    updateMagin();
+})
+
+function autoClick(){
+    curSld++;
+    console.log(curSld);
+    if (curSld > qtdSld-1){
+        curSld = 0;
+    }
+    updateMagin();
+}
+
+function updateMagin(){
+    let slideWidth = document.querySelector('.slider--item').clientWidth; //pega o width para tirar da visao
+    let newMargin = (curSld * slideWidth);
+    document.querySelector('.main--slider').style.marginLeft= `-${newMargin}px`;
+}
+
+setInterval(autoClick,6000);
